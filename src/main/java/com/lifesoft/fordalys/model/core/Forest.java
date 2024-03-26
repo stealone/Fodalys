271643c8f9254a6cde1684dfbf79162ece9e8780
Steve Alex <stevy.alon@hotmal.com>
2024-03-26 05:41:30 +0100

    The first commit

---
+++ src/main/java/com/lifesoft/fordalys/model/core/Forest.java
@@ -0,0 +1,11 @@
+package com.lifesoft.fordalys.model.core;
+
+public abstract class Forest {
+    private String forestName;
+    private int forestZone;
+
+    public Forest(String forestName){
+        this.forestName = forestName;
+    }
+
+}
